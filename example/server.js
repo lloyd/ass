7819f3df3ef1b465be2a53773a61823fd86dc67e
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-12-06 09:18:10 -0800

    stub out the example usage of ass

---
+++ example/server.js
@@ -0,0 +1,15 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+
+app.get('/hello', function(req, res){
+  res.send('Hello World');
+});
+
+app.get('/goodbye', function(req, res){
+  res.send('Goodbye Cruel World');
+});
+
+server.listen(0, '127.0.0.1', function() {
+  console.log("running on http://" +
+              server.address().address + ":" + server.address().port);
+});
